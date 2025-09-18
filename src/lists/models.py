3964eb65695b739c1bac2449df988c4da98c7312
Ghader <gharibyan.ghader@gmail.com>
2025-09-18 15:00:00 +0200

    adding database model for tokens

--- src/lists/models.py
+++ src/lists/models.py
@@ -7,3 +7,6 @@ class List(models.Model):
 class Item(models.Model):
     description = models.TextField(default='')
     list = models.ForeignKey(List,default=None, on_delete=models.CASCADE)
+class Token(models.Model):
+    email = models.EmailField()
+    uid = models.CharField(max_length=255)
