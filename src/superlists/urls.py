cd2c39c02ef6a32848176ef513e1bdd6acd6d70f
Ghader <gharibyan.ghader@gmail.com>
2025-09-19 16:00:00 +0200

    registering accounts module

--- src/superlists/urls.py
+++ src/superlists/urls.py
@@ -21,4 +21,6 @@ from lists.views import home_page
 urlpatterns = [
     path('', home_page, name='home'),
     path('lists/', include('lists.urls')),
+    path('accounts/', include('accounts.urls')),
+
 ]
