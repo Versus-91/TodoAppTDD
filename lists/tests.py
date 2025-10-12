1248bccd3752d34dab50e59f32008fa7c6b03c80
Ghader <gharibyan.ghader@gmail.com>
2025-10-12 19:33:48 +0200

    adding post request test

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.http import HttpRequest
+
+from lists.models import Item
 from lists.views import home_page
 
 # Create your tests here.
@@ -13,3 +15,13 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html_2(self):
         response = self.client.get("/")
         self.assertContains(response, "<title>To-Do lists</title>")
+
+    def test_todo_form(self):
+        response = self.client.get("/")
+        self.assertContains(response, '<form method="POST">')
+        self.assertContains(response, '<input name="todo_text"')
+    def test_save_todo(self):
+        response = self.client.post("/", data={'todo_text': "buy vegetables"})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.description, 'buy vegetables.')
