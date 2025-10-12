4f1e516cc3dd6ea0e24ca5d2c21d108b66252549
Ghader <gharibyan.ghader@gmail.com>
2025-10-12 19:33:34 +0200

    adding post request handling

--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
+from lists.models import Item
+
 
 # Create your views here.
 
 
 def home_page(req):
-    return render(req, "home.html")
+    if req.method == "POST":
+        Item.objects.create(description=req.POST['todo_text'])
+        return redirect('/')
+    #     return HttpResponse(req.POST["todo_text"])
+    return render(req, "home.html", {"new_todo_text": req.POST.get("todo_text")})
