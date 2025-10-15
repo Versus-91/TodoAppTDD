6627a4fec374523d1a9df04a5e7cb2f505db22ea
Ghader <gharibyan.ghader@gmail.com>
2025-10-15 21:07:45 +0200

    refactor functional test

---
+++ functional_tests/tests.py
@@ -0,0 +1,37 @@
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+class NewVisitorTest(LiveServerTestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_new_visitors_website_title(self):
+
+        self.browser.get(self.live_server_url)
+
+        self.assertIn("To-Do", self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, "h1").text
+        self.assertIn("To-Do", header_text)
+
+        inputbox = self.browser.find_element(By.ID, "todo_text")
+        self.assertEqual(inputbox.get_attribute(
+            "placeholder"), "Enter a to-do item")
+
+        inputbox.send_keys("Buy peacock feathers")
+
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(By.ID, "todo_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertTrue(
+            any(row.text == "1: Buy peacock feathers" for row in rows),'todo not added to the table.')
+
+
