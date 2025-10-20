219dda20c40418208a645c0056311ba4694bd859
Ghader <gharibyan.ghader@gmail.com>
2025-10-20 17:27:55 +0200

    adding tests for new route for todo item creation

--- lists/tests.py
+++ lists/tests.py
@@ -18,20 +18,21 @@ class HomePageTest(TestCase):
 
     def test_todo_form(self):
         response = self.client.get("/")
-        self.assertContains(response, '<form method="POST">')
+        self.assertContains(response, '<form method="POST"')
         self.assertContains(response, '<input name="todo_text"')
 
     def test_save_todo(self):
-        self.client.post("/", data={'todo_text': "buy vegetables"})
+        self.client.post("/lists/new", data={'todo_text': "buy vegetables"})
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.description, 'buy vegetables')
-
-
+    def test_redirects_after_POST(self):
+        response = self.client.post("/lists/new", data={"todo_text": "test todo item"})
+        self.assertRedirects(response, "/lists/unique-list/")
 class ListViewTest(TestCase):
     def test_renders_input_form(self):
         response = self.client.get("/lists/unique-list/")
-        self.assertContains(response, '<form method="POST">')
+        self.assertContains(response, '<form method="POST" action="/">')
         self.assertContains(response, '<input name="todo_text"')
 
     def test_displays_all_list_items(self):
